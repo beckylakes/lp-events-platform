faed6d535c7d5d49a59933da0c2b35747b04d8d0
beckylakes <beckylake96@gmail.com>
2024-09-17 10:46:52 +0100

    confirmed that frontend is fetching backend mock data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const App = () => {
-  return (
-    <div>
-      App
-    </div>
-  );
-}
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:9090/");
+        if (!response.ok) {
+          throw new Error(`Oh no! Something went wrong.`);
+        }
+        const data = await response.json();
+        setData(data)
+      } catch (error) {
+        console.error(`Could not fetch data: ${error.message}`);
+      }
+    };
+    fetchData();
+  }, []);
+  return <div><h1>{data.hello}</h1></div>;
+};
 
 export default App;
