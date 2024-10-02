f8af32313dc6209488c05843268ae4bb738639e0
beckylakes <beckylake96@gmail.com>
2024-10-02 16:40:32 +0100

    User schema created and MongoDB responsive

--- server/app.js
+++ server/app.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const connectDB = require("./database/connection")
+const connectDB = require("./database/connection");
+const User = require("./models/userModel");
 
 app.use(cors());
 app.use(express.json());
 
-connectDB(); 
+connectDB();
 
-app.get('/', (req, res, next) => {
-    return res.status(200).send({"hello": "world!"})
+app.get("/", (req, res, next) => {
+  return res.status(200).send({ hello: "world!" });
 });
 
 module.exports = app;
