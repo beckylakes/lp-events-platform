7ca669ec8cc2271be2ded085db317fccca0dd670
beckylakes <beckylake96@gmail.com>
2024-10-02 17:08:23 +0100

    Created eventSchema and edited userSchema

---
+++ server/models/eventModel.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+
+const eventSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    startTime: {
+      type: Date,
+      required: true,
+    },
+    endTime: {
+      type: Date,
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    price: {
+      type: Number,
+      default: 0,
+    },
+    attendees: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    isPaid: {
+      type: Boolean,
+      default: false,
+    },
+    tags: {
+      type: [String],
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+const Event = mongoose.model("Event", eventSchema);
+module.exports = Event;
