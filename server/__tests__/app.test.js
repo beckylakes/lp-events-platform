fc3ac8d31ca6b79cac5e8217e79615c03d07fce4
beckylakes <beckylake96@gmail.com>
2024-10-03 20:34:52 +0100

    Updated test suites and error message for any bad endpoint

--- server/__tests__/app.test.js
+++ server/__tests__/app.test.js
@@ -4,11 +4,12 @@ const connectDB = require("../database/connection.js");
 const seed = require("../database/seed/seed.js");
 const testData = require("../database/data/test-data/index.js");
 
+// Connect to the database before all tests
 beforeAll(async () => {
-  await connectDB(); // Connect to the database before all tests
+  await connectDB();
 });
 
-// Seed the database before each test to have a fresh state
+// Seed the database before each test
 beforeEach(async () => {
   await seed(testData);
 });
@@ -19,33 +20,72 @@ afterAll(async () => {
 });
 
 describe("GET /api/users", () => {
-  test("should respond with a 404 status code if given invalid endpoint", () => {
+    test('should respond with an array of expected length', () => {
+        return request(app)
+        .get("/api/users")
+        .expect(200)
+        .then(({body}) => {
+            const { users } = body;
+            expect(users).toBeInstanceOf(Array);
+            expect(users).toHaveLength(3);
+        })
+    });
+
+    test('each user object should have expected properties', () => {
+        return request(app)
+        .get("/api/users")
+        .expect(200)
+        .then(({body}) => {
+            const { users } = body;
+            expect(users).toHaveLength(3);
+            users.forEach((user) => {
+                expect(user).toMatchObject({
+                    _id: expect.any(String),
+                    name: expect.any(String),
+                    avatar: expect.any(String),
+                    email: expect.any(String),
+                    password: expect.any(String),
+                    role: expect.any(String),
+                    attendingEvents: expect.any(Array),
+                    createdEvents: expect.any(Array),
+                    // _v: expect.any(Date),
+                    createdAt: expect.any(String),
+                    updatedAt: expect.any(String)
+                })
+            })
+        })
+    });
+    
+    
+  test("should respond with an array of user objects", () => {
     return request(app)
-      .get("/api/uzers")
-      .expect(404)
+      .get("/api/users")
+      .expect(200)
       .then((response) => {
-        expect(response.body.msg).toBe("Page not found");
+        expect(response.body.users).toBeInstanceOf(Array);
+        expect(response.body.users.length).toBeGreaterThan(0);
+        response.body.users.forEach((user) => {
+          expect(user).toEqual(
+            expect.objectContaining({
+              name: expect.any(String),
+              email: expect.any(String),
+              role: expect.any(String),
+            })
+          );
+        });
       });
   });
-
-//   describe("GET /api/users", () => {
-//     test("should respond with an array of user objects", () => {
-//       return request(app)
-//         .get("/api/users")
-//         .expect(200)
-//         .then((response) => {
-//           expect(response.body.users).toBeInstanceOf(Array);
-//           expect(response.body.users.length).toBeGreaterThan(0);
-//           response.body.users.forEach((user) => {
-//             expect(user).toEqual(
-//               expect.objectContaining({
-//                 name: expect.any(String),
-//                 email: expect.any(String),
-//                 role: expect.any(String),
-//               })
-//             );
-//           });
-//         });
-//     });
-//   });
 });
+
+describe("GET Invalid endpoints /api/*", () => {
+    test('should return 404 and error message if given invalid endpoint', () => {
+        return request(app)
+        .get("/api/wrongendpoint")
+        .expect(404)
+        .then((response) => {
+            expect(response.body.msg).toBe("404: Page Not Found")
+        })
+    });
+    
+});
+
