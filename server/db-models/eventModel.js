c943691d38357a3292cc4b01e4285360e421efcb
beckylakes <beckylake96@gmail.com>
2024-10-05 21:07:15 +0100

    Added pictures to event schema

--- server/db-models/eventModel.js
+++ server/db-models/eventModel.js
@@ -48,6 +48,10 @@ const eventSchema = mongoose.Schema(
       type: [String],
       default: [],
     },
+    pictures: {
+      type: [String],
+      default: []
+    }
   },
   { timestamps: true }
 );
