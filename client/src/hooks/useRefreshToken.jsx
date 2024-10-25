a41437710334217547ba37e34102e453a7fd391a
beckylakes <beckylake96@gmail.com>
2024-10-25 23:39:41 +0100

    Update useRefreshToken

--- client/src/hooks/useRefreshToken.jsx
+++ client/src/hooks/useRefreshToken.jsx
@@ -1,14 +1,14 @@
 import axios from "axios";
 import { api, refreshToken } from "../api/api";
 import useAuth from "./useAuth";
+import useAxiosPrivate from "./useAxiosPrivate";
 
 const useRefreshToken = () => {
+    const axiosPrivate = useAxiosPrivate();
     const { setAuth } = useAuth();
     console.log('in useRefreshToken')
     const refresh = async () => {
-        const response = await axios.post('https://eventure-4z44.onrender.com/api/users/refresh', {}, {
-            withCredentials: true
-        });
+        const response = await axiosPrivate.post('https://eventure-4z44.onrender.com/api/users/refresh');
         console.log('in refresh')
         console.log(response)
         setAuth(prev => {
