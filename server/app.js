77c371a073f8641720bc23696cc7b2ab25cb604f
beckylakes <beckylake96@gmail.com>
2024-10-25 19:43:54 +0100

    Modified CORS for frontend access

--- server/app.js
+++ server/app.js
@@ -30,12 +30,7 @@ const {
   getTMEventById,
 } = require("./controllers/events.controllers.js");
 
-const corsOptions = {
-  origin: "http://localhost:5173",
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
