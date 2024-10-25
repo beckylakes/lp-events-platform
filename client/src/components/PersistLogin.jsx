711983ba2db8dd07cb3be879a4541a5fab2be94a
beckylakes <beckylake96@gmail.com>
2024-10-25 23:51:23 +0100

    More logs for finding error

--- client/src/components/PersistLogin.jsx
+++ client/src/components/PersistLogin.jsx
@@ -7,17 +7,20 @@ const PersistLogin = () => {
   const [isLoading, setIsLoading] = useState(true);
   const refresh = useRefreshToken();
   const { auth } = useAuth();
-
+  console.log('in PersistLogin')
   useEffect(() => {
     const verifyRefreshToken = async () => {
+      console.log('in verifyRefreshToken in PersistLogin')
       try {
+        console.log('in try block')
         await refresh();
+        console.log('after refresh')
         setIsLoading(false);
       } catch {
         setIsLoading(false)
       }
     };
-
+    console.log('after useEffect', auth)
     !auth?.accessToken ? verifyRefreshToken() : setIsLoading(false);
   }, []);
 
