ddb9ecc84e2c8dd61a530a3537733dde4476d867
beckylakes <beckylake96@gmail.com>
2024-10-26 14:17:26 +0100

    Got rid of vite config comment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   server: {
-//     proxy: {
-//       '/api': 'https://eventure-4z44.onrender.com'
-//     }
-//   },
-//   plugins: [react()],
-// })
-
 export default defineConfig({
   plugins: [react()],
 })
